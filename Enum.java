cbb9557a48587590960b25d23bcbed7783593488
Will Drescher <will_drescher@mines.edu>
2026-02-04 20:42:32 -0700

    hello

---
+++ Enum.java
@@ -0,0 +1,18 @@
+
+public class Enum{
+
+		// TODO Auto-generated constructor stub
+	public enum Temp{
+		HOT,
+		WARM,
+		COLD;
+	}
+	
+	public enum Settings{
+		OFF,
+		LOW,
+		MEDIUM,
+		HIGH;
+	}
+
+}
